e2ecaa358e5e967f53d61eb68b558f9df2bf6496
Deadshotlp <132559658+Deadshotlp@users.noreply.github.com>
2024-06-01 12:23:12 +0200

    add testwrite, add csrf exempt, modified defaulte.html

--- backend/emails/urls.py
+++ backend/emails/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("testemail", views.testemail, name="testemail"),
+    path("testemail", views.testmail, name="testemail"),
+    path("testwrite", views.testwrite, name="testwrite"),
 ]
