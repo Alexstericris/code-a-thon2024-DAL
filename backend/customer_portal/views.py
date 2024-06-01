f7ea5ec75fa3807b80a6185d136337c1ce979e8f
Alex <scristea@students.uni-mainz.de>
2024-06-01 15:37:43 +0200

    import contracts

--- backend/customer_portal/views.py
+++ backend/customer_portal/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from customer_portal.models import Company
+
 
 # Create your views here.
+def getcompanies(request):
+    companies = list(Company.objects.all().values())
+    return JsonResponse(companies, safe=False)
